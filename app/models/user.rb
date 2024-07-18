645ce75c78c4b27a1f85c2d33eb45c8ab9fe5973
hansojuhan <hjerikson@gmail.com>
2024-07-18 10:48:32 +0200

    Add Post model and controller with associations to User as author

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Has many posts as the 'author'
+  has_many :posts, foreign_key: 'author_id'
+end
